f7e32b8012b836f3d923bdfe9e1c1c8e5a57e794
ashutoshkumar1020 <115533717+ashutoshkumar1020@users.noreply.github.com>
2023-07-19 20:21:54 +0530

    done

---
+++ cerdial/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Navbar from '../src/Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* Your other website content goes here */}
+    </div>
+  );
+}
+
+export default App;
